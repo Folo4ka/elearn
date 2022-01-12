611e48cdb91956802ffabce47aa73eb7b4652774
Folo4ka <foolkoo@gmail.com>
2022-01-12 20:14:49 +0300

    cache for course detail and list

--- elearn/settings.py
+++ elearn/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'memcache_status',
     'embed_video',
     'courses',
     'students',
@@ -131,3 +132,10 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 LOGIN_REDIRECT_URL = reverse_lazy('student_course_list')
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211',
+    }
+}
